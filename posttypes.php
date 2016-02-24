31a2606179a7e07fa7cf723724e023c5285dbf39
Chris Schmieder <ckschmieder@gmail.com>
2016-02-24 11:44:15 -0500

    Initial Commit

---
+++ posttypes.php
@@ -0,0 +1,64 @@
+<?php
+/**
+* Plugin Name: CK Custom Post Types
+* Description: Creates the Testimonial Custom Post Type.
+* Author: Chris Schmieder
+* Version: 0.1
+* Author URI: http://ckschmieder.com/
+*/
+
+function ck_custom_posttypes() {
+	$labels = array(
+        'name'               => 'Testimonials',
+        'singular_name'      => 'Testimonial',
+        'menu_name'          => 'Testimonials',
+        'name_admin_bar'     => 'Testimonial',
+        'add_new'            => 'Add New',
+        'add_new_item'       => 'Add New Testimonial',
+        'new_item'           => 'New Testimonial',
+        'edit_item'          => 'Edit Testimonial',
+        'view_item'          => 'View Testimonial',
+        'all_items'          => 'All Testimonials',
+        'search_items'       => 'Search Testimonials',
+        'parent_item_colon'  => 'Parent Testimonials:',
+        'not_found'          => 'No testimonials found.',
+        'not_found_in_trash' => 'No testimonials found in Trash.',
+    );
+    
+    $args = array(
+        'labels'             => $labels,
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'menu_icon'          => 'dashicons-thumbs-up',
+        'query_var'          => true,
+        'rewrite'            => array( 'slug' => 'testimonials' ),
+        'capability_type'    => 'post',
+        'has_archive'        => true,
+        'hierarchical'       => false,
+        'menu_position'      => 5,
+        'supports'           => array( 'title', 'author', 'excerpt', 'editor', 'thumbnail' )
+    );
+
+	register_post_type( 'testimonials', $args );
+
+
+}
+
+add_action( 'init', 'ck_custom_posttypes');
+
+
+/*Flushing Rewrite on Activation (to get permalinks to work when the plugin is activated)*/
+function my_rewrite_flush() {
+    // First, we "add" the custom post type via the above written function.
+    // Note: "add" is written with quotes, as CPTs don't get added to the DB,
+    // They are only referenced in the post_type column with a post entry, 
+    // when you add a post of this CPT.
+    ck_custom_posttypes();
+
+    // ATTENTION: This is *only* done during plugin activation hook in this example!
+    // You should *NEVER EVER* do this on every page load!!
+    flush_rewrite_rules();
+}
+register_activation_hook( __FILE__, 'my_rewrite_flush' );
